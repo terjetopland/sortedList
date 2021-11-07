dd8f3e39c910dc199966c6b0f95836d1f05f7592
Terje Topland <74875770+terjetopland@users.noreply.github.com>
2021-11-07 08:34:45 +0100

    Try out to copy a pointer, didn't work yet

--- main.c
+++ main.c
@@ -11,42 +11,61 @@ int main() {
 
     printf("You wanted to enter %i integers.\nAre you sure you want this? Yes or No?\n", number_of_int);
     scanf("%s", p_yes_or_no );
-    p_yes_or_no[strlen(p_yes_or_no)-1] = '\0';
 
-    if(strcmp(p_yes_or_no, "yes") != 0) {
+
+    if(strcmp(p_yes_or_no, "yes") == 0) {
         int *p_int = malloc(number_of_int * sizeof(number_of_int));
         printf("Please enter %i integers:\n", number_of_int);
+            for (int i = 0; i < number_of_int; ++i) {
+                scanf("%i", &p_int[i]);
+            }
+
+        int *sorted_numb = malloc(number_of_int * sizeof(int));
+            //goes through the list number_of_int times
 
         for (int i = 0; i < number_of_int; ++i) {
-            scanf("%i", &p_int[i]);
+            printf(" test before loop: %i\n", sorted_numb);
         }
-        printf("Numbers in order entered: ");
-        for (int i = 0; i < number_of_int; ++i) {
-            printf("%i, \n", p_int[i]);
+            for (int outer = 0; outer < number_of_int ; ++outer) {
 
-        }
-
-        //goes through the list number_of_int times
-        for (int outer = 0; outer < number_of_int ; ++outer) {
-            //starts checking the first index with the next until all are checked
-            for (int inner = 0; inner < number_of_int-1; ++inner) {
-                int temp_store_num_to_switch;
-                if(p_int[inner] > p_int[inner + 1]) {
-                    temp_store_num_to_switch = p_int[inner];
-                    p_int[inner] = p_int[inner + 1];
-                    p_int[inner + 1] = temp_store_num_to_switch;
+                //starts checking the first index with the next until all are checked
+                for (int inner = 0; inner < number_of_int-1; ++inner) {
+                    int temp_store_num_to_switch;
+                    if(sorted_numb[inner] > sorted_numb[inner + 1]) {
+                    temp_store_num_to_switch = sorted_numb[inner];
+                    sorted_numb[inner] = sorted_numb[inner + 1];
+                    sorted_numb[inner + 1] = temp_store_num_to_switch;
                 }
             }
         }
-        printf("Numbers sorted"
-               ": ");
+
+        printf("Numbers in order entered: ");
         for (int i = 0; i < number_of_int; ++i) {
             printf("%i, ", p_int[i]);
         }
 
+        printf("\nNumbers sorted"": ");
+        for (int i = 0; i < number_of_int; ++i) {
+            printf("%i, ", sorted_numb[i]);
+        }
 
+        free(sorted_numb);
+        sorted_numb = NULL;
+        free(p_int);
+        p_int = NULL;
     }
-
+    else {
+        char yes_no[20] = { 0 };
+        printf("Do you want to enter another integer?\n");
+        fgets(yes_no, 20, stdin);
+        yes_no[strlen(yes_no) -1] = '\0';
+            if(strcmp(p_yes_or_no, "yes") == 0)
+                return  main();
+            else
+                printf("\nHave a nice day!\nGoodbye!");
+    }
+    free(p_yes_or_no);
+    p_yes_or_no = NULL;
 
     return 0;
 }
